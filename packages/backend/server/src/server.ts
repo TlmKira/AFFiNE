5a845ca666220ab8fdb9231ad7ef95d9ca2c5ac3
lizih <lizihan312@gmail.com>
2026-05-13 10:28:52 +0800

    enable gzip compression for static assets

--- packages/backend/server/src/server.ts
+++ packages/backend/server/src/server.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs';
 
@@ -84,6 +85,7 @@ export async function run() {
     app.setGlobalPrefix(config.server.path);
   }
 
+  app.use(compression({ threshold: 1024 }));
   app.use(serverTimingAndCache);
 
   app.use(
